0a2e94c30c874651df7005edbdc216f301056b4b
William A. Umaña <william.a.umana@gmail.com>
2021-11-30 07:34:27 +0100

    [app.constants] typo

---
+++ src/app/constants/api.constants.ts
@@ -0,0 +1,11 @@
+/**
+ * Constants of the API Services.
+ */
+ export class ApiConstants {
+    /** base API url */
+    public static baseUrl = 'https://jsonplaceholder.typicode.com';
+    /** Post url */
+    public static post = 'posts';
+    /** User url */
+    public static user = 'users';
+}
