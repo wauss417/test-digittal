0e25da6fbac011fee37e7ce0ed20a035335e7016
William A. Umaña <william.a.umana@gmail.com>
2021-11-30 12:23:13 +0100

    [service.user] add user requests

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+// App models
+import {
+    User
+} from '@models';
+// App constants
+import {
+    ApiConstants
+} from '@constants'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    /**
+     * Request for all Users
+     *
+     * @returns {Observable<User[]>}
+     */
+    getAllUsers(): Observable<User[]> {
+        const url = `${ApiConstants.baseUrl}/${ApiConstants.user}`;
+        return this.http.get(url, { observe: 'response' }).pipe(
+            map((data: any) => {
+                return (data.body) as User[];
+            }),
+            catchError((error: any) => {
+                return throwError(error);
+            })
+        );
+    }
+
+    /**
+     * Request to get user by id
+     *
+     * @param {numer} id user id 
+     * @returns {Observable<User>}
+     */
+    getUser(id: number): Observable<User> {
+        const url = `${ApiConstants.baseUrl}/${ApiConstants.user}/${id}`;
+        return this.http.get(url, { observe: 'response' }).pipe(
+            map((data: any) => {
+                return (data.body) as User;
+            }),
+            catchError((error: any) => {
+                return throwError(error);
+            })
+        );
+    }
 }
