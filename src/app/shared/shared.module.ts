9226d4203ea4a00cfb860ad894faad778bc25ff0
William A. Umaña <william.a.umana@gmail.com>
2021-11-30 10:14:13 +0100

    [Module: shared] add ng-material elements

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+// Material
+import { MatButtonModule } from '@angular/material/button'
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 const importsModulesShared = [
     CommonModule,
     FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
     ReactiveFormsModule,
     RouterModule
 ];
