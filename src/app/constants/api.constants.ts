2a074161d156a87105eeafc1242fdb1db493d9e2
William A. Umaña <william.a.umana@gmail.com>
2021-11-30 12:25:58 +0100

    [constants.api] add comments url

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,11 +1,13 @@
 /**
  * Constants of the API Services.
  */
- export class ApiConstants {
+export class ApiConstants {
     /** base API url */
     public static baseUrl = 'https://jsonplaceholder.typicode.com';
     /** Post url */
     public static post = 'posts';
     /** User url */
     public static user = 'users';
+    /** User url */
+    public static comment = 'comments';
 }
