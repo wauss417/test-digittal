9482df087bd8159c2f7e44c8e5fac74ad3dbccdc
William A. Umaña <william.a.umana@gmail.com>
2021-11-30 12:26:18 +0100

    [Compo: create] page to create a new post

--- src/app/modules/post/components/create/create.component.ts
+++ src/app/modules/post/components/create/create.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+
+import { Post } from '@models';
+import { PostService } from '@services';
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+    selector: 'app-create',
+    templateUrl: './create.component.html',
+    styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    public post: Post = null;
+    private subscriptions: Subscription[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private postService: PostService
+    ) { }
 
+
+    ngOnInit(): void {
+    }
+
+    createPost(form: Partial<Post>) {
+        form.userId = 1;
+        this.subscriptions.push(
+            this.postService.createPost(form).subscribe(
+                (resp: Post) => {
+                    this.post = resp
+                },
+                (e: any) => {
+                    this.post = null;
+                    console.error('Cant get post', e)
+                }
+            )
+        )
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach(e => e.unsubscribe());
+    }
 }
